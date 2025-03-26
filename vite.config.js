1b7a82db41beb4bf1b372a4bf113b37c6168d3d9
kamils007 <136223329+kamils007@users.noreply.github.com>
2025-03-26 10:51:17 +0100

    Trigger GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
   base: '/pepubetv3-frontend/', // 👈 to jest kluczowe!
   plugins: [react()],
